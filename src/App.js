c2804c922adade4f2f628d669d137fc99f90a616
Petr Reshetin <preshetin@gmail.com>
2018-06-08 17:27:50 +0300

    Training start

--- src/App.js
+++ src/App.js
@@ -3,15 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      trainingString: "fff ddd fff ddd",
+      currentIndex: 0
+    }
+    this.keyPressed = this.keyPressed.bind(this);
+  }
+
+  keyPressed(event){
+    const char = event.key;
+    if (char === this.state.trainingString[this.state.currentIndex]) {
+      console.log(char, "correct");
+    } else {
+      console.log(char, "wrong");
+    }
+    let st = this.state;
+    st.currentIndex++;
+    this.setState(st);
+  }
+
+  componentDidMount(){
+    document.addEventListener("keydown", this.keyPressed, false);
+  }
+  componentWillUnmount(){
+    document.removeEventListener("keydown", this.keyPressed, false);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Typing Training</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <code>{this.state.trainingString}</code>
         </p>
       </div>
     );
