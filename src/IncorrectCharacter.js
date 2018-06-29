16667d1038e5488ee0e631b73a9a17b482838533
Petr Reshetin <preshetin@gmail.com>
2018-06-29 16:42:02 +0300

    Incorrect character fixes

--- src/IncorrectCharacter.js
+++ src/IncorrectCharacter.js
@@ -4,20 +4,33 @@ class IncorrectCharacter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      incorrectSymbol: null,
       visible: true
     }
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ visible: false });
-    }, 200)
-   }
+    this.setState(prevState => ({
+      ...prevState,
+        incorrectSymbol: this.props.incorrectSymbol
+    }));
+
+    this.timeout = setTimeout(() => {
+      this.setState(prevState => ({
+        ...prevState,
+        visible: false
+      }));
+    }, 300)
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
   
   render() {
     return <div style={{ display: "inline", position: "relative" }}>
               <div className="Character" style={{ backgroundColor: "red" }}>{this.props.symbol}</div>
-              <div className={this.state.visible ? 'Character' : 'Character hide'} style={{ color: "red", position: "absolute", top: "11px", left: 0 }}>{this.props.incorrectSymbol}</div>
+              <div className={this.state.visible ? 'Character' : 'Character hide'} style={{ color: "red", position: "absolute", top: "11px", left: 0 }}>{this.state.incorrectSymbol}</div>
            </div>;
   }
 }
