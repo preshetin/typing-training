78ae7412469e1c0d5b6f14b54ccfd3b6f1759226
Petr Reshetin <preshetin@gmail.com>
2018-06-30 20:36:37 +0300

    Fake auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Link } from "react-router-dom";
 import TrainingRoom from './TrainingRoom';
 import LessonsList from './LessonsList';
+import Login from './Login';
 import Header from './Layouts/Header';
 import Footer from './Layouts/Footer';
 import './App.css';
+import { fakeAuth } from './auth';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      fakeAuth.isAuthenticated ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
+
+const Profile = () => <h3>Profile</h3>;
 
 class App extends Component {
   constructor(props) {
@@ -12,9 +34,6 @@ class App extends Component {
   }
 
   render() {
-    const result =  <TrainingRoom />;
-    //    const result =  <LessonsList />;
-
     return (
       <Router>
         <div>
@@ -22,6 +41,8 @@ class App extends Component {
           <div className="container">
             <Route exact path="/" component={LessonsList} />
             <Route path="/lessons/1" component={TrainingRoom} />
+            <Route path="/login" component={Login} />
+            <PrivateRoute path="/profile" component={Profile} />
           </div>
         </div>
       </Router>
--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -1,5 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { fakeAuth } from '../auth';
+
+const AuthButton = withRouter(
+  ({ history }) =>
+    fakeAuth.isAuthenticated ? (
+      <p>
+        Welcome!{" "}
+        <button
+          onClick={() => {
+            fakeAuth.signout(() => history.push("/"));
+          }}
+        >
+          Sign out
+        </button>
+      </p>
+    ) : (
+      <p>You are not logged in.</p>
+    )
+);
 
 class Header extends React.Component {
   render() {
@@ -7,10 +26,9 @@ class Header extends React.Component {
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
       <h5 className="my-0 mr-md-auto font-weight-normal"><Link to="/">Typing Training</Link></h5>
       <nav className="my-2 my-md-0 mr-md-3">
-        <a className="p-2 text-dark" href="#">Features</a>
-        <a className="p-2 text-dark" href="#">Enterprise</a>
-        <a className="p-2 text-dark" href="#">Support</a>
-        <a className="p-2 text-dark" href="#">Pricing</a>
+        <AuthButton />
+        <Link className="p-2" to="/profile">Profile</Link>
+        <Link className="p-2" to="/login">Login</Link>
       </nav>
       <a className="btn btn-outline-primary" href="#">Sign up</a>
     </div>
