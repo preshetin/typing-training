4722639515cd2f9a440461fa0102e59e913bf714
Petr Reshetin <preshetin@gmail.com>
2018-07-02 16:31:54 +0300

    Authentication

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const token = localStorage.getItem('token') || null;
+const token = localStorage.getItem('token');
 
 export const api = axios.create({
  baseURL: 'http://localhost:3000/api',
