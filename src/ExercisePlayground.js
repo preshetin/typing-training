a3f79c6fff9869902fab2582947df08e7accb9a3
Petr Reshetin <preshetin@gmail.com>
2018-07-04 12:45:14 +0300

    Split code refactoring

---
+++ src/ExercisePlayground.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import TypingStrings from './TypingStrings';
+import ExerciseResult from './ExerciseResult';
+import Keyboard from './Keyboard';
+import { isExercisePassed, correctRate, exerciseIsFinished, getCharByGlobalIndex, getArrayIndexByGlobalIndex, getStringIndexByGlobalIndex } from './arrayConverter.js';
+
+class ExercisePlayground extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chars: [],
+      currentIndex: 0,
+      currentSymbol: ""
+    };
+  }
+
+  prepareExerciseChars(exercise) {
+    const chars = [];
+    exercise.forEach((str, index) => {
+      chars.push([]);
+      str.split('').forEach((symbol, charIndex) => {
+        chars[index].push({ symbol, typeStatus: null })
+        if (str.length === charIndex + 1) {
+          chars[index].push({ symbol: " ", typeStatus: null });
+        }
+      });
+    });
+    return chars;
+  }
+
+  handleCharacterType = (symbol) => {
+    let st = this.state;
+    if (symbol === getCharByGlobalIndex(this.state.chars, this.state.currentIndex).symbol) {
+     if (st.chars[getArrayIndexByGlobalIndex(this.state.chars, this.state.currentIndex)][getStringIndexByGlobalIndex(this.state.chars, this.state.currentIndex)].typeStatus === 'fixing') {
+       st.chars[getArrayIndexByGlobalIndex(this.state.chars, this.state.currentIndex)][getStringIndexByGlobalIndex(this.state.chars, this.state.currentIndex)].typeStatus = 'fixed';
+     } else {
+     st.chars[getArrayIndexByGlobalIndex(this.state.chars, this.state.currentIndex)][getStringIndexByGlobalIndex(this.state.chars, this.state.currentIndex)].typeStatus = 'correct';
+     }
+    } else {
+     st.chars[getArrayIndexByGlobalIndex(this.state.chars, this.state.currentIndex)][getStringIndexByGlobalIndex(this.state.chars, this.state.currentIndex)].typeStatus = 'mistake';
+    }
+    st.currentIndex++;
+    st.currentSymbol = symbol;
+    this.setState(st);
+  }
+
+  handleBackspaceType = (symbol) => {
+    let st = this.state;
+      if (st.currentIndex > 0) {
+        st.currentIndex--;
+        if (st.chars[getArrayIndexByGlobalIndex(st.chars, st.currentIndex)][getStringIndexByGlobalIndex(st.chars, st.currentIndex)].typeStatus === 'mistake' || st.chars[getArrayIndexByGlobalIndex(st.chars, st.currentIndex)][getStringIndexByGlobalIndex(st.chars, st.currentIndex)].typeStatus === 'fixed') {
+          st.chars[getArrayIndexByGlobalIndex(st.chars, st.currentIndex)][getStringIndexByGlobalIndex(st.chars, st.currentIndex)].typeStatus = 'fixing';
+        } else {
+          st.chars[getArrayIndexByGlobalIndex(st.chars, st.currentIndex)][getStringIndexByGlobalIndex(st.chars, st.currentIndex)].typeStatus = null;
+        }
+      }
+    this.setState(st);
+    return;
+  }
+
+  prepareNextExercise = () => {
+    this.props.onNext();
+    this.prepareComponent();
+  }
+
+  prepareTryAgain = () => {
+    this.prepareComponent();
+  }
+
+  componentDidMount() {
+    this.prepareComponent();
+  }
+
+  prepareComponent() {
+    let st = JSON.parse(JSON.stringify(this.state));
+    st.chars = this.prepareExerciseChars(this.props.exercise.task);
+    st.currentIndex = 0;
+    st.currentSymbol = "";
+    this.setState(st);
+  }
+
+  render() {
+    if (this.state.chars.length === 0) {
+      return "Loading...";
+    }
+    
+    if (exerciseIsFinished(this.state.chars, this.state.currentIndex)) {
+      this.props.onFinish(correctRate(this.state.chars));
+      return <ExerciseResult 
+        correctRate={correctRate(this.state.chars)} 
+        onTryAgain={this.prepareTryAgain} 
+        onNextExercice={this.prepareNextExercise}
+        />;
+    }
+
+    return (
+      <React.Fragment>
+          <div className="row">
+            <div className="col">
+            <div className="card ">
+              <div className="card-body">
+                <TypingStrings 
+                  chars={this.state.chars} 
+                  currentIndex={this.state.currentIndex} 
+                  currentSymbol={this.state.currentSymbol}
+                  onCharacterType={this.handleCharacterType}
+                  onBackspaceType={this.handleBackspaceType} 
+                />
+              </div>
+            </div>   
+            </div>   
+          </div>
+          <div className="row">
+            <Keyboard lang="en" hide/>
+          </div>
+          <br />
+      </React.Fragment>
+    )
+  }
+}
+
+export default ExercisePlayground;
--- src/TrainingRoom.js
+++ src/TrainingRoom.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Api from './api';
-import ExerciseResult from './ExerciseResult';
+import ExercisePlayground from './ExercisePlayground';
 import ExerciseNav from './ExerciseNav';
-import TypingStrings from './TypingStrings';
-import Keyboard from './Keyboard';
-import { isExercisePassed, correctRate, exerciseIsFinished, getCharByGlobalIndex, getArrayIndexByGlobalIndex, getStringIndexByGlobalIndex } from './arrayConverter.js';
 import { prepareExercises } from './utils';
 
 class TrainingRoom extends React.Component {
@@ -13,43 +10,9 @@ class TrainingRoom extends React.Component {
     super(props);
     this.state = {
       lesson: null,
-      exercises: [],
-      chars: [],
-      currentIndex: 0,
-      exerciseIndex: 0,
-      currentSymbol: ""
+      exercises: null,
+      exerciseIndex: 0
     }
-  }
-
-  prepareExerciseChars(exercise) {
-    const chars = [];
-    exercise.forEach((str, index) => {
-      chars.push([]);
-      str.split('').forEach((symbol, charIndex) => {
-        chars[index].push({ symbol, typeStatus: null })
-        if (str.length === charIndex + 1) {
-          chars[index].push({ symbol: " ", typeStatus: null });
-        }
-      });
-    });
-    return chars;
-  }
-
-  prepareNextExercise = () => {
-    this.prepareExersiseAtIndex(this.state.exerciseIndex + 1);
-  }
-
-  prepareTryAgain = () => {
-    this.prepareExersiseAtIndex(this.state.exerciseIndex);
-  }
-
-  prepareExersiseAtIndex(index) {
-    const st = this.state;
-    st.currentIndex = 0;
-    st.currentSymbol = "";
-    st.exerciseIndex = index;
-    st.chars = this.prepareExerciseChars(st.exercises[st.exerciseIndex].task);
-    this.setState(st);
   }
 
   componentDidMount() {
@@ -57,82 +20,36 @@ class TrainingRoom extends React.Component {
     api.getLessonExercisesAndLog(this.props.match.params.lessonId, (exersices, lessonLog) => {
       let st = JSON.parse(JSON.stringify(this.state));
       st.exercises = prepareExercises(exersices);
-      st.chars = this.prepareExerciseChars(st.exercises[0].task);
       st.lessonLog = lessonLog;
       this.setState(st);
     });
   }
 
-  handleCharacterType = (symbol) => {
-    let st = this.state;
-    if (symbol === getCharByGlobalIndex(this.state.chars, this.state.currentIndex).symbol) {
-     if (st.chars[getArrayIndexByGlobalIndex(this.state.chars, this.state.currentIndex)][getStringIndexByGlobalIndex(this.state.chars, this.state.currentIndex)].typeStatus === 'fixing') {
-       st.chars[getArrayIndexByGlobalIndex(this.state.chars, this.state.currentIndex)][getStringIndexByGlobalIndex(this.state.chars, this.state.currentIndex)].typeStatus = 'fixed';
-     } else {
-     st.chars[getArrayIndexByGlobalIndex(this.state.chars, this.state.currentIndex)][getStringIndexByGlobalIndex(this.state.chars, this.state.currentIndex)].typeStatus = 'correct';
-     }
-    } else {
-     st.chars[getArrayIndexByGlobalIndex(this.state.chars, this.state.currentIndex)][getStringIndexByGlobalIndex(this.state.chars, this.state.currentIndex)].typeStatus = 'mistake';
-    }
-    st.currentIndex++;
-    st.currentSymbol = symbol;
+  handleFinish(correctRate) {
+    console.log('finished', correctRate);
+  }
+
+  prepareNextExercise = () => {
+    let st = JSON.parse(JSON.stringify(this.state));
+    st.exerciseIndex++;
     this.setState(st);
   }
 
-  handleBackspaceType = (symbol) => {
-    let st = this.state;
-      if (st.currentIndex > 0) {
-        st.currentIndex--;
-        if (st.chars[getArrayIndexByGlobalIndex(st.chars, st.currentIndex)][getStringIndexByGlobalIndex(st.chars, st.currentIndex)].typeStatus === 'mistake' || st.chars[getArrayIndexByGlobalIndex(st.chars, st.currentIndex)][getStringIndexByGlobalIndex(st.chars, st.currentIndex)].typeStatus === 'fixed') {
-          st.chars[getArrayIndexByGlobalIndex(st.chars, st.currentIndex)][getStringIndexByGlobalIndex(st.chars, st.currentIndex)].typeStatus = 'fixing';
-        } else {
-          st.chars[getArrayIndexByGlobalIndex(st.chars, st.currentIndex)][getStringIndexByGlobalIndex(st.chars, st.currentIndex)].typeStatus = null;
-        }
-      }
-    this.setState(st);
-    return;
-  }
-
-  saveLessonLog() {
-    
+  prepareTryAgain = () => {
   }
 
   render() {
-    if (this.state.chars.length === 0) {
+    if (this.state.exercises == null) {
       return "Loading...";
-    }
-    
-    if (exerciseIsFinished(this.state.chars, this.state.currentIndex)) {
-      this.saveLessonLog();
-
-      return <ExerciseResult 
-        correctRate={correctRate(this.state.chars)} 
-        onTryAgain={this.prepareTryAgain} 
-        onNextExercice={this.prepareNextExercise}
-        />;
     }
 
     return (
       <div>
-        <div className="row">
-          <div className="col">
-          <div className="card ">
-            <div className="card-body">
-              <TypingStrings 
-                chars={this.state.chars} 
-                currentIndex={this.state.currentIndex} 
-                currentSymbol={this.state.currentSymbol}
-                onCharacterType={this.handleCharacterType}
-                onBackspaceType={this.handleBackspaceType} 
-              />
-            </div>
-          </div>   
-          </div>   
-        </div>
-        <div className="row">
-          <Keyboard lang="en" hide/>
-        </div>
-        <br />
+        <ExercisePlayground
+          exercise={this.state.exercises[this.state.exerciseIndex]}
+          onFinish={this.handleFinish} 
+          onNext={this.prepareNextExercise}
+        />
         <div className="row">
           <div className="col">
             <ExerciseNav exercises={this.state.exercises} activeIndex={this.state.exerciseIndex} />
