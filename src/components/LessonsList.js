2c0d8a607df3498c95cc5a04b1bfe74f98a7f9a5
Petr Reshetin <preshetin@gmail.com>
2018-07-17 11:42:16 +0300

    Login alert at home page

--- src/components/LessonsList.js
+++ src/components/LessonsList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LessonCard from './LessonCard';
+import IsLoggedInAlert from './IsLoggedInAlert';
 
 class LessonsList extends React.Component {
   constructor(props) {
@@ -16,17 +17,24 @@ class LessonsList extends React.Component {
 
   render() {
     return (
-      <div className="row justify-content-md-center">
-          <div className="card-deck text-center">
-            {this.props.lessons.map(lesson => (
-              <div key={lesson.id} className="col-md-6" style={{ marginBottom: "25px" }}>
-                <LessonCard
-                  lesson={lesson}
-                  completeRate={this.getCorrectRateForLesson(lesson)}
-                />
-              </div>
-            ))}
+      <div>
+        <div className="row justify-content-md-center">
+          <div className="col-md-12" >
+              <IsLoggedInAlert authUser={this.props.authUser} />
           </div>
+        </div>
+        <div className="row justify-content-md-center">
+            <div className="card-deck text-center">
+              {this.props.lessons.map(lesson => (
+                <div key={lesson.id} className="col-md-6" style={{ marginBottom: "25px" }}>
+                  <LessonCard
+                    lesson={lesson}
+                    completeRate={this.getCorrectRateForLesson(lesson)}
+                  />
+                </div>
+              ))}
+            </div>
+        </div>
       </div>
     );
   }
